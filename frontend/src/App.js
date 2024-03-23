d3838670bc30f9f43159483f9ad3a9f0623bb13b
tshekerli <tshekerli@gmail.com>
2024-03-23 15:48:19 +0400

    Add MyComponent to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,45 +6,19 @@ import Check_box from './components/checkbox';
 import PaxSelect from './components/paxSelect';
 import DatePickerDrawer from './components/flexibleDates';
 import BasicSelect from './components/currency';
+import MyComponent from './components/widget';
 
 
 
 
 function App() {
-  const [isChecked, setIsChecked] = useState(false);
-
-  const handleCheckChange = (newCheckedState) => {
-    setIsChecked(newCheckedState);
-  };
-
-  const handleValueChange = (value) => {
-    console.log(value);
-  };
+  
  
   return (
-    <div>
-      <Check_box onCheckChange={handleCheckChange} />
-      {isChecked ? (
-        <LocationSelect
-          id="departure-location"
-          label="Please select your departure location"
-          onValueChange={handleValueChange}
-        />
-      ) : (
-        <LocationSelect
-          id="arrival-location"
-          label="Please select your arrival location"
-          onValueChange={handleValueChange}
-        />
-      )}
-
-<PaxSelect onPaxChange={(newPax) => console.log(newPax)} />
-
-<DatePickerDrawer />
-
-<BasicSelect  />
+    <div className="App">
+      <MyComponent/>
     </div>
-  );
+  )
 }
 
 export default App;
