75a1d5b455f3d82ff554b1bc6d56a7dbd22ae736
tshekerli <tshekerli@gmail.com>
2024-04-17 10:21:13 +0400

    Update frontend dependencies and add react-router-dom package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
-
 import { SearchUi } from './components/SearchUi';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home"
+import Search from "./pages/search"
+import About from "./pages/about"
+
 
 
 function App() {
@@ -8,7 +13,15 @@ function App() {
   
  
   return (
+    
     <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element = {<Home/>}/>
+        <Route path='/search' element = {<Search/>}/>
+        <Route path='/about' element = {<About/>}/>
+      </Routes>
+      </BrowserRouter>
       <SearchUi/>
     </div>
   )
