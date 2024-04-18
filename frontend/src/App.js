2d7913bcb7aeeb59b7e115853a1fda8b5652caef
tshekerli <tshekerli@gmail.com>
2024-04-18 10:21:39 +0400

    Update frontend styles, refactor components, and add react-router-dom package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { SearchUi } from './components/SearchUi';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import Home from "./pages/home"
 import Search from "./pages/search"
 import About from "./pages/about"
+import Navbar from './components/navbar';
 
 
 
@@ -15,14 +16,20 @@ function App() {
   return (
     
     <div className="App">
-      <BrowserRouter>
+      
+      
+      
+      {/* <SearchUi/> */}
+
+      <Navbar/>
+
       <Routes>
         <Route path='/' element = {<Home/>}/>
         <Route path='/search' element = {<Search/>}/>
         <Route path='/about' element = {<About/>}/>
       </Routes>
-      </BrowserRouter>
-      <SearchUi/>
+
+
     </div>
   )
 }
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,21 +1,62 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, NavLink, useLocation } from "react-router-dom";
+import "./navbar.css";
+import Logo from "../img/logo.jpg";
+import NotFoundPage from './NotFoundPage'; // import your 404 page
 
 function Navbar() {
+  const location = useLocation();
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <img src={Logo} alt="" />
+          </li>
+          <li>
+            <NavLink 
+              style={{textDecoration:'none', color:"#747474"}} 
+              isActive={() => location.pathname === "/"}
+              activeStyle={{pointerEvents: 'none', color: '#747474'}} 
+              to="/"
+              exact
+            >
+              Home
+            </NavLink>
+          </li>
+          
+          <li>
+            <NavLink 
+              style={{textDecoration:'none', color:"#747474"}} 
+              isActive={() => location.pathname === "/search"}
+              activeStyle={{pointerEvents: 'none', color: '#747474'}} 
+              to="/search"
+            >
+              Search
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink 
+              style={{textDecoration:'none', color:"#747474"}} 
+              isActive={() => location.pathname === "/about"}
+              activeStyle={{pointerEvents: 'none', color: '#747474'}} 
+              to="/about"
+            >
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/search" component={SearchPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Router>
   );
 }
 
