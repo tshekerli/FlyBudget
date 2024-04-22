ec5c9f7f32e7b20ed66beb47d9bddaa3e8be1105
tshekerli <tshekerli@gmail.com>
2024-04-22 19:58:19 +0400

    Refactor frontend components, update styles, and fix typos in results.css and results.jsx

--- frontend/src/components/results.jsx
+++ frontend/src/components/results.jsx
@@ -15,6 +15,10 @@ import Eleventh from "../img/results/11.jpg";
 import Twelfth from "../img/results/12.jpg";
 import Thirteenth from "../img/results/13.jpg";
 import { Dimmer, Loader, Image, Segment, Modal } from "semantic-ui-react";
+import Slide from '@mui/material/Slide';
+import Grow from '@mui/material/Grow';
+import SlidingDiv from "./slidingimage"
+
 import "./results.css";
 
 const images = [
@@ -86,6 +90,11 @@ function formatNumber(num) {
 export function ResultContainer({ data, isLoading }) {
   const [open, setOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
+  
+
+  
+
+   
 
   const handleOpen = (item) => {
     setSelectedItem(item);
@@ -102,22 +111,7 @@ export function ResultContainer({ data, isLoading }) {
 
   if (!data) {
     return (
-      <div
-        style={{
-          position: "relative",
-          alignSelf: "flex-start",
-          marginTop: "3rem",
-          boxShadow: "0px 4px 8px 0px rgba(0,0,0,0.2)",
-          borderRadius:"10%"
-        }}
-      >
-        <img
-          src={EmptySearchImage}
-          alt="Please search"
-          height={"500px"}
-          style={{ borderRadius: "10%" }}
-        />
-      </div>
+      <SlidingDiv/>
     );
   }
 
@@ -160,6 +154,7 @@ export function ResultContainer({ data, isLoading }) {
         }}
       >
         {data.map((item, index) => (
+          <Grow in={true} style={{ transformOrigin: '0 0 0' }} timeout={index *600}>
           <div
             key={index}
             className="result-div"
@@ -213,6 +208,7 @@ export function ResultContainer({ data, isLoading }) {
               </a>
             </div>
           </div>
+          </Grow>
         ))}
         {selectedItem && (
   <Modal
@@ -242,27 +238,30 @@ export function ResultContainer({ data, isLoading }) {
       flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
-      textAlign: 'center'
+      textAlign: 'center',
+      gap:"10px"
     }}>
       {selectedItem.routes.map((route, routeIndex) => {
         const departureDate = new Date(route.localDeparture);
         return (
           <div key={routeIndex}>
-            <p>
+            <p id="modal-from" style={{backgroundColor:"rgba(0, 0, 0, 0.5)", color: "white", borderRadius:"500px",  }}>
               {route.cityFrom} - {route.cityTo}
             </p>
-            <p>{departureDate.toLocaleString()}</p>
+            <p id="modal-time" style={{backgroundColor:"rgba(0, 0, 0, 0.5)", color: "white", borderRadius:"500px",  }}>{departureDate.toLocaleString()}</p>
           </div>
         );
       })}
-      <p className="price-info secondary-data"> 
+      <p id="modal-price"  style={{backgroundColor:"rgba(0, 0, 0, 0.5)", color: "white", borderRadius:"500px",  }} className="price-info secondary-data"> 
         {formatNumber(selectedItem.price)} {selectedItem.currency}
       </p>
       <a
         href={selectedItem.link}
-        className="search-link secondary-data" 
+        className="search-link secondary-data"
+        id="modal-link" 
         target="_blank"
-        style={{ color: "white" }}
+        
+        style={{backgroundColor:"rgba(0, 0, 0, 0.5)", color: "white", borderRadius:"500px",  }}
       >
         Link
       </a>
