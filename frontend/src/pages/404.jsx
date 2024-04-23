932f4264a1e84a654d6185833080353092188785
tshekerli <tshekerli@gmail.com>
2024-04-23 18:42:02 +0400

    Refactor frontend components, update styles, and fix typos in results.css and results.jsx

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -1,23 +1,53 @@
-import NotFoundImage from "../img/404404.png"
-import ButtonComponent from '../components/button';
-import { useNavigate } from 'react-router-dom';
+import NotFoundImage from "../img/404404.png";
+import ButtonComponent from "../components/button";
+import { useNavigate } from "react-router-dom";
+import SlidingImage from "../components/Sliding404image";
+import Footer from "../components/footer";
+import { WidthFull } from "@mui/icons-material";
+import Navbar from "../components/navbar";
 
+function NotFoundPage() {
+  const navigate = useNavigate();
 
+  const TakeMeHome = () => {
+    navigate("/");
+  };
 
-
-function NotFoundPage () {
-    const navigate = useNavigate();
-
-    const TakeMeHome = () => {
+  return (
+    <div className="page404"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height:"100vh",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0",
+        overflow:"hidden"
         
-        navigate('/')}
-
-    return (
-        <div style={{display: "flex", justifyContent: "center", alignItems: "center", height:"100vh", flexDirection:"column", gap:"2rem"}}>
-            <img src={NotFoundImage} width={"500px"} alt="" style={{borderRadius: "10%"}} />
-            <ButtonComponent id="takemehome" text={"Take Me Home, Country Roads "} onClick={TakeMeHome} style={{borderRadius:"500px", color:"#747474", fontSize:"1.25rem"}}/>
-        </div>
-    )
+      }}
+    >
+        
+      <div className="img404">
+        <SlidingImage />
+      </div>
+      <div style={{height:"150px"}}>
+        <ButtonComponent
+          id="takemehome"
+          text={"Take Me Home, Country Roads "}
+          onClick={TakeMeHome}
+          style={{
+            borderRadius: "500px",
+            color: "#747474",
+            fontSize: "1.25rem",
+          }}
+        />
+      </div>
+      <div style={{ width: "100%", alignSelf:"flex-end" }}>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
-export default NotFoundPage
+export default NotFoundPage;
